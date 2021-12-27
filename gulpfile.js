cb6db88302709aa623fa35ccf7851aea31879bb4
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2021-12-27 20:47:20 +0200

    add header

--- gulpfile.js
+++ gulpfile.js
@@ -15,23 +15,23 @@ gulp.task('server', function () {
 });
 
 gulp.task('styles', function () {
-  return gulp.src("/src/scss/**/*.+(scss|sass)")
-    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-    .pipe(rename({
-      prefix: "",
-      suffix: ".min",
-    }))
-    .pipe(autoprefixer({
-      cascade: false
-    }))
-    .pipe(cleanCSS({ compatibility: 'ie8' }))
-    .pipe(gulp.dest('/src/css'))
-    .pipe(browserSync.stream());
+	return gulp.src("./src/scss/**/*.+(scss|sass)")
+		.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+		.pipe(rename({
+			prefix: "",
+			suffix: ".min",
+		}))
+		.pipe(autoprefixer({
+			cascade: false
+		}))
+		.pipe(cleanCSS({ compatibility: 'ie8' }))
+		.pipe(gulp.dest('./src/css'))
+		.pipe(browserSync.stream());
 })
 
 gulp.task('watch', function () {
-  gulp.watch("scss/*.+(scss|sass)", gulp.parallel("styles"))
-  gulp.watch("./*.html").on("change", browserSync.reload);
+	gulp.watch("./src/scss/**/*.+(scss|sass)", gulp.parallel("styles"))
+	gulp.watch("./src/*.html").on("change", browserSync.reload);
 })
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'))
