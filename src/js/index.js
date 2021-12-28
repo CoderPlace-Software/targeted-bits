c2e38f7591e0db1b5b2dee6a2416b3b9231751a7
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2021-12-28 19:03:04 +0200

    mobile menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-const init = () => {
+const initMobileMenu = () => {
   const mobileMenuOpenEl = document.getElementById("mobile-menu-open");
   const mobileMenuCloseEl = document.getElementById("mobile-menu-close");
   const mobileMenuEl = document.getElementById("mobile-menu");
@@ -9,4 +9,23 @@ const init = () => {
   mobileMenuCloseEl.addEventListener("click", toggleMenu);
 };
 
-init();
+const initHeader = () => {
+  const headerEl = document.getElementById("header");
+  const headerSmallClass = "header__small";
+
+  const handleHeaderClass = () => {
+    const currentScroll =
+      document.documentElement.scrollTop || document.body.scrollTop;
+
+    if (currentScroll > 50) {
+      headerEl.classList.add(headerSmallClass);
+    } else {
+      headerEl.classList.remove(headerSmallClass);
+    }
+  };
+
+  window.addEventListener("scroll", handleHeaderClass, false);
+};
+
+initMobileMenu();
+initHeader();
